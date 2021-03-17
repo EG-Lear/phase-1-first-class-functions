7198bdc87e0fbfd3929bef1e5a1f78010532cb5a
Garret <gwiats@gmail.com>
2021-03-17 18:20:34 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(x) {
+    x()
+}
+
+function returnsANamedFunction() {
+    return function test() {
+        console.log('test')
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return (() => console.log('test'))
+}
